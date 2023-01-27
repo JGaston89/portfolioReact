484c21f9014acbd8759e9479d34337efcd544736
JGaston89 <gastonjurado89@gmail.com>
2023-01-26 21:33:52 -0300

    contact and footer changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './contact.css'
+import {AiOutlineMail} from 'react-icons/ai'
+import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs  from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_7hbiboi', 'template_o3ei4yr', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get in touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <AiOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>gastonjurado89@gmail.com</h5>
+            <a href="mailto:gastonjurado89@gmail.com" target="_blank">Send a menssaje</a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+5492213544972</h5>
+            <a href="http://api.whatsapp.com/send?phone=+5492213544972" target="_blank">Send a menssaje</a>
+          </article>
+        </div>
+        {/* END OF CONTACT */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Mensaje' required></textarea>
+          <button type='submit' className='btn btn-primary'> Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
